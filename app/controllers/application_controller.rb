f298328629820c1cbcd5d518c13467bf7b81cf0f
Kamil Tarnowski <kam.tarnowski@gmail.com>
2015-04-10 14:30:07 +0200

    Minor fixes. Before creating mail form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_cache_buster
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-store"
+  end
 
   protected
 
